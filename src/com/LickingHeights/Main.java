855fa1cf8b0db653b79d9b6e66334a6f850db170
Eli Spicer <joshua.spicer.2020@student.lhschools.org>
2020-02-25 12:14:25 -0500

    intellij.commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,26 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is");
+        System.out.println("Eli");
+
+
+
+
+        System.out.print("I Played video games,");
+
+
+
+
+        System.out.print(" slept a lot,");
+
+
+
+
+        System.out.print(" and went over to my grandparents house over summer break.");
+    }
+
+}
